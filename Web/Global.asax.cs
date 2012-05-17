2342315d9b219cd2098342532b925fe912cb07f3
mmartynets <marina.martinec@gmail.com>
2012-05-17 20:32:51 +0400

    тестовый коммит

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -20,7 +20,7 @@ namespace Marina.Store.Web
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults.
             );
 
         }
