8cf97a99d87c5d5d37188a2c510f80dd2fd5b1dc
sscherbakov <sscherbakov@rapidsoft.ru>
2012-05-30 17:13:31 +0400

    Mapped entities, updated ListProductsByCategory command

---
+++ Web/Commands/GetShoppingCartCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Marina.Store.Web.DataAccess;
+using Marina.Store.Web.Models;
+
+namespace Marina.Store.Web.Commands
+{
+    public class GetShoppingCartCommand : Command
+    {
+        private readonly StoreDbContext _db;
+        private readonly User _user;
+
+        public GetShoppingCartCommand(StoreDbContext db, User user)
+        {
+            _db = db;
+            _user = user;
+        }
+
+        public CommandResult<ShoppingCart> Execute()
+        {
+            var cart = _db.ShoppingCarts.Include("Items").First(c => c.User.Id == _user.Id);
+            return Result(cart);
+        }
+    }
+}
