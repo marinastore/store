188c408046db50ef8ff5537f506a7578837ab199
sscherbakov <sscherbakov@rapidsoft.ru>
2012-06-25 23:06:58 +0400

    Moved EF model building to mapping classes

---
+++ Web/DataAccess/Mappings/AddressMapping.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Marina.Store.Web.Models;
+
+namespace Marina.Store.Web.DataAccess.Mappings
+{
+    public class AddressMapping : EntityTypeConfiguration<Address>
+    {
+        public AddressMapping()
+        {
+            Property(a => a.Metro).IsOptional().HasMaxLength(30);
+            Property(a => a.City).IsRequired().HasMaxLength(30);
+            Property(a => a.StreetAddress).IsRequired().HasMaxLength(200);
+        }
+    }
+}
---
+++ Web/DataAccess/Mappings/CategoryMapping.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Marina.Store.Web.Models;
+
+namespace Marina.Store.Web.DataAccess.Mappings
+{
+    public class CategoryMapping : EntityTypeConfiguration<Category>
+    {
+        public CategoryMapping()
+        {
+            Property(a => a.Name).IsRequired().HasMaxLength(100);
+            Property(a => a.Description).IsOptional().HasMaxLength(500);
+            Property(a => a.Picture).IsOptional().HasMaxLength(100);
+        }
+    }
+}
