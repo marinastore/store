1497c81123f8936bdc4ffc06e9036d4dea283311
sscherbakov <sscherbakov@rapidsoft.ru>
2012-07-18 16:53:08 +0400

    Добавил заглушку для сайта (Home/Index), чтобы подготовить проект к стейджингу

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Marina.Store.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Content("[ Я  &mdash; заглушка будущего великого проекта ]");
+        }
+
+    }
+}
